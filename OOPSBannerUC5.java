5d305225b118de44cc4f8ecd42a194bc428aa1f5
Varshinie Machha <vm2482@srmist.edu.in>
2026-03-02 08:27:11 +0530

    initial commit of uc5

---
+++ OOPSBannerUC5.java
@@ -0,0 +1,54 @@
+public class OOPSBannerUC5 {
+
+    public static void main(String[] args) {
+
+        String[] banner = {
+
+                String.join("  ",
+                        " ***** ",
+                        " ***** ",
+                        " ******",
+                        " ***** "),
+
+                String.join("  ",
+                        "*     *",
+                        "*     *",
+                        "*     *",
+                        "*      "),
+
+                String.join("  ",
+                        "*     *",
+                        "*     *",
+                        "*     *",
+                        "*      "),
+
+                String.join("  ",
+                        "*     *",
+                        "*     *",
+                        " ******",
+                        " ***** "),
+
+                String.join("  ",
+                        "*     *",
+                        "*     *",
+                        "*      ",
+                        "      *"),
+
+                String.join("  ",
+                        "*     *",
+                        "*     *",
+                        "*      ",
+                        "      *"),
+
+                String.join("  ",
+                        " ***** ",
+                        " ***** ",
+                        "*      ",
+                        " ***** ")
+        };
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
