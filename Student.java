29b23623944af73404ba2089b218bd5586d8de65
Varshinie Machha <vm2482@srmist.edu.in>
2026-03-11 09:25:08 +0530

    initial commit of newuc1

---
+++ Student.java
@@ -0,0 +1,18 @@
+public class Student{
+	public static void main(String[] args) {
+		int[] ar = new int[5];
+		ar[0]= 10;
+		ar[1]= 20;
+		ar[2]= 30;
+		ar[3]= 40;
+		ar[4]= 50;
+		
+		System.out.println(ar[0]);
+		System.out.println(ar[1]);
+		System.out.println(ar[2]);
+		System.out.println(ar[3]);
+		System.out.println(ar[4]);
+	}
+	
+}
+		
